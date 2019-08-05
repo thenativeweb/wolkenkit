8a239423308c5bd483c1a8eac816cbd004dc2e07
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2019-08-05 11:24:31 +0200

    Add types to omitByDeep.

--- src/common/utils/omitByDeep.ts
+++ src/common/utils/omitByDeep.ts
@@ -1,4 +1,4 @@
-import _, { cloneDeepWith, omitBy, isArray, isObject } from 'lodash';
+import _, { isArray, isObject } from 'lodash';
 
 const omitByDeep = function (object: any, predicate: (value: any, key: string) => boolean): any {
   if (!object || !isObject(object) || isArray(object)) {
@@ -7,42 +7,8 @@ const omitByDeep = function (object: any, predicate: (value: any, key: string) =
 
   return _(object).
     omitBy(predicate).
-    mapValues(value => omitByDeep(value, predicate)).
+    mapValues((value: any): any => omitByDeep(value, predicate)).
     value();
 };
 
-// const omitByDeep = function <T extends object> (object: T | undefined, predicate: (value: T[keyof T], key: string) => boolean): T | undefined {
-//   if (!object || !isObject(object) || isArray(object)) {
-//     return object;
-//   }
-//
-//   return _(object).
-//     omitBy(predicate).
-//     mapValues(value => omitByDeep(value, predicate)).
-//     value();
-// };
-
-// const omitByDeep = function <T extends object> (object: T, predicate: (value: T[keyof T], key: string) => boolean) {
-//   const omitFn = function (value: T) {
-//     const result = omitBy(value, predicate);
-//
-//     return omitByDeep(result, predicate);
-//   }
-//
-//   return cloneDeepWith(object, omitFn);
-// }
-
 export default omitByDeep;
-
-
-const foo = {
-  a: 23,
-  b: 42,
-  c: {
-    d: 68
-  }
-};
-
-const result = omitByDeep(foo, (value, key) => value > 28);
-
-console.log('#####', result);
