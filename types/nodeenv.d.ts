8066c4843b7eb91a58ac6264f23853cfb2dcf804
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2019-08-28 15:19:16 +0200

    Replace nodeenv and uuidv4 type declarations with package updates.

--- types/nodeenv.d.ts
+++
@@ -1,7 +0,0 @@
-import { Dictionary } from '../src/types/Dictionary';
-
-declare function nodeenv(envVars: Dictionary<any>): () => void;
-declare function nodeenv(envVarName: string, envVarValue: any): () => void;
-declare function nodeenv(nodeEnv: any): () => void;
-
-export default nodeenv;
