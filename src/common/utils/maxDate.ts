b96c7574778bc8f48e4ca8545dfb8adb0d04f535
Golo Roden <golo.roden@thenativeweb.io>
2019-10-11 09:14:01 +0200

    Improve comments.

--- src/common/utils/maxDate.ts
+++ src/common/utils/maxDate.ts
@@ -1,14 +1,15 @@
-// This value represents the maximum possible date in JavaScript. For details
-// see: http://ecma-international.org/ecma-262/5.1/#sec-15.9.1.1
+// The maximum possible date in JavaScript. For details see
+// http://ecma-international.org/ecma-262/5.1/#sec-15.9.1.1
 const javascript = 8_640_000_000_000_000;
 
-// Max MariaDB timestamp is 9999-12-31 23:59:59.
+// The maximum possible date in MariaDB is 9999-12-31 23:59:59.
 const mariaDb = 253_402_297_199_000;
 
-// Max MySQL timestamp is 9999-12-31 23:59:59.
+// The maximum possible date in MySQL is 9999-12-31 23:59:59.
 const mySql = 253_402_297_199_000;
 
-// Max SqlServer datetime2 is 9999-12-31 23:59:59.
+// The maximum possible date in Sql Server is 9999-12-31 23:59:59, using a
+// datetime2 column.
 const sqlServer = 253_402_297_199_000;
 
 export default javascript;
