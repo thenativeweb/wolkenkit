caf0127453f5f03778f8a0b04f02b81e98b72618
Golo Roden <golo.roden@thenativeweb.io>
2019-10-11 12:02:07 +0200

    Fix import path.

--- src/clis/wolkenkit/configuration/latest/depot/container.js
+++ src/clis/wolkenkit/configuration/latest/depot/container.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 const get = require('lodash/get');
 
-const image = require('./image');
+const errors = require('../../../../../common/errors'),
+      image = require('./image');
 
 const container = function ({
   configuration,
