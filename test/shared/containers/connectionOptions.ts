e0e0530c39051f91c41eb98b8aa65558348ed9d6
Golo Roden <golo.roden@thenativeweb.io>
2019-10-15 01:02:27 +0200

    Finally remove keys and remove Consul configuration.

--- test/shared/containers/connectionOptions.ts
+++ test/shared/containers/connectionOptions.ts
@@ -1,11 +1,4 @@
 export const connectionOptions = {
-  consul: {
-    hostname: 'localhost',
-    portApi: 8500,
-    portDns: 8600,
-    encryptConnection: false,
-    externalDns: '8.8.8.8'
-  },
   mariaDb: {
     hostname: 'localhost',
     port: 3306,
