370ed857613548530bacbd5fe033f097632dba69
Golo Roden <golo.roden@thenativeweb.io>
2019-10-19 23:39:56 +0200

    Remove the explicitly created store.

--- __temp__/sampleApplicationWithTypeScript/server/views/games/index.ts
+++ __temp__/sampleApplicationWithTypeScript/server/views/games/index.ts
@@ -1,8 +1,7 @@
 import { PlayingGameOpened } from './projections/PlayingGameOpened';
-import { Store } from './Store';
 import * as All from './queries/All';
 import * as Top50 from './queries/Top50';
 
-export const store = new Store();
+export { Store } from './Store';
 export const projections = { PlayingGameOpened };
 export const queries = { All, Top50 };
