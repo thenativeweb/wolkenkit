42587123fdbc2f622372c9a1de4e6962c33449b5
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2019-10-25 10:17:42 +0200

    Disable eslint for newly found issue regarding unbound methods.

--- test/unit/common/services/LoggerServiceTests.ts
+++ test/unit/common/services/LoggerServiceTests.ts
@@ -6,11 +6,13 @@ suite('LoggerService', (): void => {
   test('provides logger functions.', async (): Promise<void> => {
     const loggerService = getLoggerService({ fileName: __filename });
 
+    /* eslint-disable @typescript-eslint/unbound-method */
     assert.that(loggerService.fatal).is.ofType('function');
     assert.that(loggerService.error).is.ofType('function');
     assert.that(loggerService.warn).is.ofType('function');
     assert.that(loggerService.info).is.ofType('function');
     assert.that(loggerService.debug).is.ofType('function');
+    /* eslint-enable @typescript-eslint/unbound-method */
   });
 
   test('logs with the provided file name.', async (): Promise<void> => {
