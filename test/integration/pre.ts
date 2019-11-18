87a976238619990aacd5a8c5a0132fe50536e7dd
Golo Roden <golo.roden@thenativeweb.io>
2019-11-18 10:33:34 +0100

    Make pre and post fail on error. (#282)

--- test/integration/pre.ts
+++ test/integration/pre.ts
@@ -1,18 +1,28 @@
+#!/usr/bin/env node
+
 import { buildImages } from '../../docker/buildImages';
+import { flaschenpost } from 'flaschenpost';
 import { mariaDb, minio, mongoDb, mySql, postgres, redis, sqlServer } from '../shared/containers';
 
 /* eslint-disable @typescript-eslint/no-floating-promises */
 (async function (): Promise<void> {
-  await buildImages();
+  const logger = flaschenpost.getLogger();
 
-  await Promise.all([
-    mariaDb.start(),
-    minio.start(),
-    mongoDb.start(),
-    mySql.start(),
-    postgres.start(),
-    redis.start(),
-    sqlServer.start()
-  ]);
+  try {
+    await buildImages();
+
+    await Promise.all([
+      mariaDb.start(),
+      minio.start(),
+      mongoDb.start(),
+      mySql.start(),
+      postgres.start(),
+      redis.start(),
+      sqlServer.start()
+    ]);
+  } catch (ex) {
+    logger.fatal('An unexpected error occured.', { ex });
+    process.exit(1);
+  }
 })();
 /* eslint-enable @typescript-eslint/no-floating-promises */
