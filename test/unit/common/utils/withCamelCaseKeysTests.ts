75fb12d257e6d7a4e2e6a0d9c725ce0ad1b87640
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2019-12-02 15:51:40 +0100

    Rename test file and improve test names.

--- test/unit/common/utils/withCamelCaseKeysTests.ts
+++ test/unit/common/utils/withCamelCaseKeysTests.ts
@@ -2,11 +2,11 @@ import { assert } from 'assertthat';
 import { withCamelCaseKeys } from '../../../../lib/common/utils/withCamelCaseKeys';
 
 suite('withCamelCaseKeys', (): void => {
-  test('it does not do anything to an empty object.', async (): Promise<void> => {
+  test('does not do anything to an empty object.', async (): Promise<void> => {
     assert.that(withCamelCaseKeys({})).is.equalTo({});
   });
 
-  test('it converts all keys in an object to camel case.', async (): Promise<void> => {
+  test('converts all keys in an object to camel case.', async (): Promise<void> => {
     const object = {
       foo: 'foo',
       FOO_BAR: 'fooBar',
