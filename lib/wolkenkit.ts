482caca0a4b4bd7870d2b60d3f132adc1501668e
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2020-02-17 17:32:55 +0100

    feat: Add the services to the exports; Use ErrorService in test application.

--- lib/wolkenkit.ts
+++ lib/wolkenkit.ts
@@ -1,4 +1,7 @@
 import { Aggregate } from './common/elements/Aggregate';
+import { AggregateService } from './common/services/AggregateService';
+import { AggregatesService } from './common/services/AggregatesService';
+import { ClientService } from './common/services/ClientService';
 import { Command } from './common/elements/Command';
 import { CommandData } from './common/elements/CommandData';
 import { CommandHandler } from './common/elements/CommandHandler';
@@ -7,7 +10,10 @@ import { DomainEvent } from './common/elements/DomainEvent';
 import { DomainEventData } from './common/elements/DomainEventData';
 import { DomainEventHandler } from './common/elements/DomainEventHandler';
 import { DomainEventWithState } from './common/elements/DomainEventWithState';
+import { ErrorService } from './common/services/ErrorService';
 import { GetInitialState } from './common/elements/GetInitialState';
+import { LockService } from './common/services/LockService';
+import { LoggerService } from './common/services/LoggerService';
 import { ProjectionHandler } from './common/elements/ProjectionHandler';
 import { QueryHandler } from './common/elements/QueryHandler';
 import { QueryOptions } from './common/elements/QueryOptions';
@@ -19,6 +25,9 @@ import { ViewInitializer } from './common/elements/ViewInitializer';
 
 export {
   Aggregate,
+  AggregateService,
+  AggregatesService,
+  ClientService,
   Command,
   CommandData,
   CommandHandler,
@@ -27,7 +36,10 @@ export {
   DomainEventData,
   DomainEventHandler,
   DomainEventWithState,
+  ErrorService,
   GetInitialState,
+  LockService,
+  LoggerService,
   ProjectionHandler,
   QueryHandler,
   QueryOptions,
