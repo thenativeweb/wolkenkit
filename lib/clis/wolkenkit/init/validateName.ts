885d57ca8ed67fef35063221695f33ff1c63ce75
Golo Roden <golo.roden@thenativeweb.io>
2020-03-06 07:10:43 +0100

    fix: Support numbers in application names.

--- lib/clis/wolkenkit/init/validateName.ts
+++ lib/clis/wolkenkit/init/validateName.ts
@@ -2,7 +2,7 @@ import { nameRegularExpression } from './nameRegularExpression';
 
 const validateName = function (value: string): void {
   if (!nameRegularExpression.test(value)) {
-    throw new Error(`Name must only contain lowercase characters and dashes.`);
+    throw new Error('Name must be a valid npm package name.');
   }
 };
 
