86eb7433200c6aad69a78a6add0273c5915ec545
Golo Roden <golo.roden@thenativeweb.io>
2020-06-08 22:11:37 +0200

    Fix paths.

--- test/unit/stores/lockStore/shared/getHashTests.ts
+++ test/unit/stores/lockStore/shared/getHashTests.ts
@@ -1,5 +1,5 @@
 import { assert } from 'assertthat';
-import { getHash } from '../../../../lib/stores/lockStore/getHash';
+import { getHash } from '../../../../../lib/stores/lockStore/shared/getHash';
 
 suite('getHash', (): void => {
   test('returns the SHA256 hash for the given value.', async (): Promise<void> => {
