243c43d600eacf1aed39910f4ef9ebd7af1b5b79
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2020-06-22 13:01:05 +0200

    chore: Add infrastructure to all test applications.

---
+++ test/shared/applications/typescript/base/server/infrastructure/index.ts
@@ -0,0 +1,10 @@
+import { AskInfrastructure, TellInfrastructure } from 'wolkenkit';
+
+const getInfrastructure = async function (): Promise<AskInfrastructure & TellInfrastructure> {
+  return {
+    ask: {},
+    tell: {}
+  };
+}
+
+export { getInfrastructure }
