7ea0df45f482c4de8e8ad1f5ff9dfc54fe19ba18
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2020-07-07 17:50:25 +0200

    fix: Fix type issues in sandboxes; Add sandbox tests. (#1005)
    * feat: Execute flow for multiple domain events in sandbox.
    
    * 4.0.0-internal.40
    
    * fix: Fix broken types in sandbox for flow.
    
    * 4.0.0-internal.41
    
    * chore: Add sandbox tests for flows in all application templates.
    
    * Add missing semicolons.
    
    Co-authored-by: Golo Roden <golo.roden@thenativeweb.io>

--- lib/common/utils/test/sandbox/SandboxForFlow.ts
+++ lib/common/utils/test/sandbox/SandboxForFlow.ts
@@ -4,53 +4,40 @@ import { DomainEventData } from '../../../elements/DomainEventData';
 import { Initiator } from '../../../elements/Initiator';
 
 export interface SandboxForFlow {
-  given <TDomainEventData extends DomainEventData>(parameters: {
-    contextIdentifier: { name: string };
-    aggregateIdentifier: { name: string; id: string };
-    name: string;
-    data: TDomainEventData;
-    id?: string;
-    metadata?: {
-      causationId?: string;
-      correlationId?: string;
-      timestamp?: number;
-      initiator?: Initiator;
-      tags?: string[];
-    };
-  }): SandboxForFlow;
-
-  and <TDomainEventData extends DomainEventData>(parameters: {
-    contextIdentifier: { name: string };
-    aggregateIdentifier: { name: string; id: string };
-    name: string;
-    data: TDomainEventData;
-    id?: string;
-    metadata?: {
-      causationId?: string;
-      correlationId?: string;
-      timestamp?: number;
-      initiator?: Initiator;
-      tags?: string[];
-    };
-  }): SandboxForFlow;
-
   when <TDomainEventData extends DomainEventData>(parameters: {
     contextIdentifier: { name: string };
     aggregateIdentifier: { name: string; id: string };
     name: string;
     data: TDomainEventData;
     id?: string;
-    metadata?: {
+    metadata: {
       causationId?: string;
       correlationId?: string;
       timestamp?: number;
       initiator?: Initiator;
       tags?: string[];
+      revision: number;
     };
   }): SandboxForFlowWithResult;
 }
 
 export interface SandboxForFlowWithResult {
+  and <TDomainEventData extends DomainEventData>(parameters: {
+    contextIdentifier: { name: string };
+    aggregateIdentifier: { name: string; id: string };
+    name: string;
+    data: TDomainEventData;
+    id?: string;
+    metadata: {
+      causationId?: string;
+      correlationId?: string;
+      timestamp?: number;
+      initiator?: Initiator;
+      tags?: string[];
+      revision: number;
+    };
+  }): SandboxForFlowWithResult;
+
   then(callback: ((parameters: {
     commands: Command<CommandData>[];
   }) => void | Promise<void>)): Promise<void>;
