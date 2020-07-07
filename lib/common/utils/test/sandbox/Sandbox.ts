1d9d618439b78cc8237089b6b0e618cd8e5f2a4c
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2020-07-07 14:18:38 +0200

    feat: Implement sandbox for flows. (#999)
    * fix: Fix typo in sandbox.
    
    * chore: Add type for command service factory.
    
    * feat: Implement flow sandbox.
    
    * feat: Export flow sandbox in entrypoint.
    
    * feat: Merge common stuff of sandboxes; Refactor elements into multiple files.
    
    * fix: Clean up interfaces.
    
    * fix: Exports in entry point.
    
    * fix: Import of sandbox in test.

---
+++ lib/common/utils/test/sandbox/Sandbox.ts
@@ -0,0 +1,73 @@
+import { AggregateIdentifier } from '../../../elements/AggregateIdentifier';
+import { Application } from '../../../application/Application';
+import { ConsumerProgressStore } from '../../../../stores/consumerProgressStore/ConsumerProgressStore';
+import { ContextIdentifier } from '../../../elements/ContextIdentifier';
+import { DomainEventStore } from '../../../../stores/domainEventStore/DomainEventStore';
+import { GetAggregateService } from '../../../services/types/GetAggregateService';
+import { GetAggregatesService } from '../../../services/types/GetAggregatesService';
+import { GetClientService } from '../../../services/types/GetClientService';
+import { GetCommandService } from '../../../services/types/GetCommandService';
+import { GetLockService } from '../../../services/types/GetLockService';
+import { GetLoggerService } from '../../../services/types/GetLoggerService';
+import { LockStore } from '../../../../stores/lockStore/LockStore';
+import { SandboxForAggregate } from './SandboxForAggregate';
+import { SandboxForFlow } from './SandboxForFlow';
+import { SnapshotStrategy } from '../../../domain/SnapshotStrategy';
+import { State } from '../../../elements/State';
+
+export interface UninitializedSandbox {
+  withApplication({ application }: {
+    application: Application;
+  }): Sandbox;
+}
+
+export interface Sandbox {
+  withDomainEventStore(parameters: {
+    domainEventStore: DomainEventStore;
+  }): Sandbox;
+
+  withFlowProgressStore(parameters: {
+    flowProgressStore: ConsumerProgressStore;
+  }): Sandbox;
+
+  withLockStore(parameters: {
+    lockStore: LockStore;
+  }): Sandbox;
+
+  withSnapshotStrategy(parameters: {
+    snapshotStrategy: SnapshotStrategy;
+  }): Sandbox;
+
+  withAggregateServiceFactory(parameters: {
+    aggregateServiceFactory: GetAggregateService;
+  }): Sandbox;
+
+  withAggregatesServiceFactory(parameters: {
+    aggregatesServiceFactory: GetAggregatesService;
+  }): Sandbox;
+
+  withClientServiceFactory(parameters: {
+    clientServiceFactory: GetClientService;
+  }): Sandbox;
+
+  withCommandServiceFactory(parameters: {
+    commandServiceFactory: GetCommandService;
+  }): Sandbox;
+
+  withLockServiceFactory(parameters: {
+    lockServiceFactory: GetLockService;
+  }): Sandbox;
+
+  withLoggerServiceFactory(parameters: {
+    loggerServiceFactory: GetLoggerService;
+  }): Sandbox;
+
+  forAggregate<TState extends State>(parameters: {
+    contextIdentifier: ContextIdentifier;
+    aggregateIdentifier: AggregateIdentifier;
+  }): SandboxForAggregate<TState>;
+
+  forFlow(parameters: {
+    flowName: string;
+  }): SandboxForFlow;
+}
