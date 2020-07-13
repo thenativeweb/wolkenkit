1a3636cb30f8e21a59643dd51b56411dbf2dc145
Golo Roden <golo.roden@thenativeweb.io>
2020-07-13 13:25:22 +0200

    Add client for querying views.

---
+++ lib/apis/queryView/http/v2/Client.ts
@@ -0,0 +1,79 @@
+import axios from 'axios';
+import { errors } from '../../../../common/errors';
+import { flaschenpost } from 'flaschenpost';
+import { HttpClient } from '../../../shared/HttpClient';
+import { QueryDescription } from '../../../../common/application/QueryDescription';
+import { PassThrough, pipeline } from 'stream';
+
+const logger = flaschenpost.getLogger();
+
+class Client extends HttpClient {
+  public constructor ({ protocol = 'http', hostName, port, path = '/' }: {
+    protocol?: string;
+    hostName: string;
+    port: number;
+    path?: string;
+  }) {
+    super({ protocol, hostName, port, path });
+  }
+
+  public async getDescription (): Promise<Record<string, Record<string, QueryDescription>>> {
+    const { data, status } = await axios({
+      method: 'get',
+      url: `${this.url}/description`,
+      validateStatus (): boolean {
+        return true;
+      }
+    });
+
+    if (status === 200) {
+      return data;
+    }
+
+    logger.error('An unknown error occured.', { ex: data, status });
+
+    throw new errors.UnknownError();
+  }
+
+  public async query ({ viewName, queryName, options = {}}: {
+    viewName: string;
+    queryName: string;
+    options?: object;
+  }): Promise<PassThrough> {
+    const { data, status } = await axios({
+      method: 'get',
+      url: `${this.url}/${viewName}/${queryName}`,
+      params: { options },
+      paramsSerializer (params): string {
+        return Object.entries(params).
+          map(([ key, value ]): string => `${key}=${JSON.stringify(value)}`).
+          join('&');
+      },
+      responseType: 'stream',
+      validateStatus (): boolean {
+        return true;
+      }
+    });
+
+    if (status !== 200) {
+      logger.error('An unknown error occured.', { ex: data, status });
+
+      throw new errors.UnknownError();
+    }
+
+    const passThrough = new PassThrough({ objectMode: true });
+
+    return pipeline(
+      data,
+      passThrough,
+      (err): void => {
+        if (err) {
+          // Do not handle errors explicitly. The returned stream will just close.
+          logger.error('An error occured during stream piping.', { err });
+        }
+      }
+    );
+  }
+}
+
+export { Client };
