218fdf63d06079b26987c056fac5896a864b8ce7
Golo Roden <golo.roden@thenativeweb.io>
2020-08-04 15:42:48 +0200

    fix: Make directory non-optional for filesystem file store. (#1195)

--- test/integration/stores/fileStore/FileSystemTests.ts
+++ test/integration/stores/fileStore/FileSystemTests.ts
@@ -1,11 +1,15 @@
 import { FileStore } from '../../../../lib/stores/fileStore/FileStore';
 import { FileSystemFileStore } from '../../../../lib/stores/fileStore/FileSystem';
 import { getTestsFor } from './getTestsFor';
+import { isolated } from 'isolated';
 
 suite('FileSystem', (): void => {
   getTestsFor({
     async createFileStore (): Promise<FileStore> {
-      return await FileSystemFileStore.create({ type: 'FileSystem' });
+      return await FileSystemFileStore.create({
+        type: 'FileSystem',
+        directory: await isolated()
+      });
     }
   });
 });
