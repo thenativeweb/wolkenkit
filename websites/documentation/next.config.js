ceff833c50ea2ad9ec1a566dec81b6a752efb018
Golo Roden <golo.roden@thenativeweb.io>
2020-08-19 11:15:58 +0200

    chore: Update dependencies. (#1257)
    * chore: Update dependencies.
    
    * Update configuration.

--- websites/documentation/next.config.js
+++ websites/documentation/next.config.js
@@ -6,7 +6,7 @@ const libraryEntryPointPath = path.resolve(__dirname, '..', '..', 'lib');
 
 module.exports = {
   // We dont't want Next.js to create pages like components.html but components/index.html.
-  exportTrailingSlash: true,
+  trailingSlash: true,
   webpack (configuration) {
     configuration.module.rules[0].include.push(libraryEntryPointPath);
 
