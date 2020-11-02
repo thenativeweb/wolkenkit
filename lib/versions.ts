033c253a3afc03926923b879aa12011c746a5019
Golo Roden <golo.roden@thenativeweb.io>
2020-11-02 17:01:30 +0100

    Fix TypeScript version.

--- lib/versions.ts
+++ lib/versions.ts
@@ -4,7 +4,7 @@ const versions = {
     'docker-compose': '3.7'
   },
   packages: {
-    typescript: '3.9.7'
+    typescript: '4.0.5'
   },
   dockerImages: {
     minio: 'RELEASE.2019-10-12T01-39-57Z',
