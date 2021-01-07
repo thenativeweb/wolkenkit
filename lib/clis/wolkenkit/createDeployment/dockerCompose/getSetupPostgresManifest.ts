5d9bf2382c93c685d5f2e17b27b33a5f0017709d
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2021-01-07 16:51:54 +0100

    fix: Fix formatting in setup manifest.

--- lib/clis/wolkenkit/createDeployment/dockerCompose/getSetupPostgresManifest.ts
+++ lib/clis/wolkenkit/createDeployment/dockerCompose/getSetupPostgresManifest.ts
@@ -14,17 +14,17 @@ const getSetupPostgresManifest = function (): string {
     services:
       setup:
         build: '../../'
-          command: >
-            sh -c "
-              node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup store consumer-progress postgres --host-name ${services.postgres.hostName} --port ${services.postgres.privatePort} --user-name ${services.postgres.userName} --password ${services.postgres.password} --database ${services.postgres.database} --table-name-progress progress-flow &&
-              node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup store domain-event postgres --host-name ${services.postgres.hostName} --port ${services.postgres.privatePort} --user-name ${services.postgres.userName} --password ${services.postgres.password} --database ${services.postgres.database} --table-name-domain-events domainEvents --table-name-snapshots snapshots &&
-              node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup store file s3 --host-name ${services.minio.hostName} --port ${services.minio.privatePort} --access-key ${services.minio.accessKey} --secret-key ${services.minio.secretKey} --bucket-name ${services.minio.bucketName} &&
-              node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup store lock postgres --host-name ${services.postgres.hostName} --port ${services.postgres.privatePort} --user-name ${services.postgres.userName} --password ${services.postgres.password} --database ${services.postgres.database} --table-name-locks locks &&
-              node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup store priority-queue postgres --host-name ${services.postgres.hostName} --port ${services.postgres.privatePort} --user-name ${services.postgres.userName} --password ${services.postgres.password} --database ${services.postgres.database} --table-name-items items-command --table-name-priority-queue priorityQueue-command &&
-              node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup store priority-queue postgres --host-name ${services.postgres.hostName} --port ${services.postgres.privatePort} --user-name ${services.postgres.userName} --password ${services.postgres.password} --database ${services.postgres.database} --table-name-items items-domain-event --table-name-priority-queue priorityQueue-domain-event &&
-              node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup infrastructure
-            "
-          init: true
+        command: >
+          sh -c "
+            node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup store consumer-progress postgres --host-name ${services.postgres.hostName} --port ${services.postgres.privatePort} --user-name ${services.postgres.userName} --password ${services.postgres.password} --database ${services.postgres.database} --table-name-progress progress-flow &&
+            node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup store domain-event postgres --host-name ${services.postgres.hostName} --port ${services.postgres.privatePort} --user-name ${services.postgres.userName} --password ${services.postgres.password} --database ${services.postgres.database} --table-name-domain-events domainEvents --table-name-snapshots snapshots &&
+            node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup store file s3 --host-name ${services.minio.hostName} --port ${services.minio.privatePort} --access-key ${services.minio.accessKey} --secret-key ${services.minio.secretKey} --bucket-name ${services.minio.bucketName} &&
+            node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup store lock postgres --host-name ${services.postgres.hostName} --port ${services.postgres.privatePort} --user-name ${services.postgres.userName} --password ${services.postgres.password} --database ${services.postgres.database} --table-name-locks locks &&
+            node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup store priority-queue postgres --host-name ${services.postgres.hostName} --port ${services.postgres.privatePort} --user-name ${services.postgres.userName} --password ${services.postgres.password} --database ${services.postgres.database} --table-name-items items-command --table-name-priority-queue priorityQueue-command &&
+            node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup store priority-queue postgres --host-name ${services.postgres.hostName} --port ${services.postgres.privatePort} --user-name ${services.postgres.userName} --password ${services.postgres.password} --database ${services.postgres.database} --table-name-items items-domain-event --table-name-priority-queue priorityQueue-domain-event &&
+            node ./node_modules/wolkenkit/build/lib/bin/wolkenkit.js setup infrastructure
+          "
+        init: true
   `;
 };
 
