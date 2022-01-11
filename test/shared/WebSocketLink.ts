9a9e33736f3181eeb54a31fe8d63c005bcb8abe0
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2022-01-11 10:19:28 +0100

    fix: WebSocketLink errors in runtime tests.

--- test/shared/WebSocketLink.ts
+++ test/shared/WebSocketLink.ts
@@ -27,23 +27,17 @@ class WebSocketLink extends ApolloLink {
             return sink.error(err);
           }
 
-          if (err instanceof CloseEvent) {
+          // Dirty hack to check if err is a CloseEvent, since CloseEvent is not
+          // available in Node.js
+          if (Object.getPrototypeOf(err as any)[Symbol.toStringTag] === 'CloseEvent') {
             return sink.error(
 
               // Reason will be available on clean closes
               new Error(
-                `Socket closed with event ${err.code} ${err.reason || ''}`
+                `Socket closed with event ${(err as CloseEvent).code} ${(err as CloseEvent).reason || ''}`
               )
             );
           }
-
-          sink.error(
-            new Error(
-              (err as GraphQLError[]).
-                map(({ message }): string => message).
-                join(', ')
-            )
-          );
         }
       }
     ));
